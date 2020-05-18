6100e4f6a4dfd7a3cea13251a1545fe3a913e406
Niels Kelchtermans <kelchtermansniels@gmail.com>
2020-05-18 22:21:21 +0200

    Add project files.

---
+++ BIBData/Models/BIBDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+
+namespace BIBData.Models
+{
+    public class BIBDbContext: DbContext
+    {
+        public DbSet<Lener> Leners { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+            optionsBuilder.UseSqlServer(
+            configuration.GetConnectionString("BIBConnection"));
+        }
+
+    }
+}
---
+++ BIBData/Models/Lener.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BIBData.Models
+{
+    public class Lener
+    {
+        public int Id { get; set; }
+        public string Voornaam { get; set; }
+        public string Familienaam { get; set; }
+        public string Adres { get; set; }
+        public DateTime Geboortedatum { get; set; }
+        public string Telefoonnr { get; set; }
+    }
+}
